90423acd9badb50ab907f8650a18bfcc686c5505
Andrei Zinovyev <andrei.zinovyev@curie.fr>
2021-08-18 02:26:58 +0200

    add synthetic examples

---
+++ datasets/synthetic/3clusters/3sticks/test_matlab_code.m
@@ -0,0 +1,7 @@
+X = load('C:\Datas\DAPCA\3clusters\3sticks\X.csv');
+labels = load('C:\Datas\DAPCA\3clusters\3sticks\labels.csv');
+figure; [v,u,s] = pca(X); scatter(u(:,1),u(:,2),10,labels,'filled'); title('Normal PCA');
+figure; [ V, D ] = SupervisedPCA(X, labels, 2, 'usual'); u = X*V; scatter(u(:,1),u(:,2),10,labels,'filled'); title('Usual PCA');
+figure; [ V, D ] = SupervisedPCA(X, labels, 2, 'super'); u = X*V; scatter(u(:,1),u(:,2),10,labels,'filled'); title('Supervised PCA');
+figure; [ V, D ] = SupervisedPCA(X, labels, 2, 'supernorm'); u = X*V; scatter(u(:,1),u(:,2),10,labels,'filled'); title('Supervised normalized PCA');
+figure; alpha = 1; [ V, D ] = SupervisedPCA(X, labels, 2, alpha); u = X*V; scatter(u(:,1),u(:,2),10,labels,'filled'); title(strcat('Supervised advanced PCA, alpha=',num2str(alpha)));
