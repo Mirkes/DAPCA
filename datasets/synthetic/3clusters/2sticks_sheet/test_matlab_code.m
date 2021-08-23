13453e0f0bf9daa95105cab36e31f263de019819
Andrei Zinovyev <andrei.zinovyev@curie.fr>
2021-08-23 21:09:36 +0200

    Update test_matlab_code.m

--- datasets/synthetic/3clusters/2sticks_sheet/test_matlab_code.m
+++ datasets/synthetic/3clusters/2sticks_sheet/test_matlab_code.m
@@ -1,7 +1,11 @@
 X = load('C:\Datas\DAPCA\3clusters\2sticks_sheet\X.csv');
 labels = load('C:\Datas\DAPCA\3clusters\2sticks_sheet\labels.csv');
+Y = load('C:\Datas\DAPCA\3clusters\2sticks_sheet\Y.csv');
+labelsY = -ones(3000,1);
+labels_all = [labels;labelsY];
 figure; [v,u,s] = pca(X); scatter(u(:,1),u(:,2),10,labels,'filled'); title('Normal PCA');
 figure; [ V, D ] = SupervisedPCA(X, labels, 2, 'usual'); u = X*V; scatter(u(:,1),u(:,2),10,labels,'filled'); title('Usual PCA');
 figure; [ V, D ] = SupervisedPCA(X, labels, 2, 'super'); u = X*V; scatter(u(:,1),u(:,2),10,labels,'filled'); title('Supervised PCA');
 figure; [ V, D ] = SupervisedPCA(X, labels, 2, 'supernorm'); u = X*V; scatter(u(:,1),u(:,2),10,labels,'filled'); title('Supervised normalized PCA');
 figure; alpha = 1; [ V, D ] = SupervisedPCA(X, labels, 2, alpha); u = X*V; scatter(u(:,1),u(:,2),10,labels,'filled'); title(strcat('Supervised advanced PCA, alpha=',num2str(alpha)));
+figure; alpha = 1; [ V, D ] = SupervisedPCA(XX, labels_all, 2, alpha); u = XX*V; scatter(u(:,1),u(:,2),10,labels_all,'filled'); title(strcat('Supervised advanced PCA, alpha=',num2str(alpha)));
