84cfb5506eff7afd80914f92421b823a96804d48
Andrei Zinovyev <andrei.zinovyev@curie.fr>
2021-11-17 23:08:51 +0100

    report on a bug

--- datasets/MNIST_M/DAPCA_error.m
+++ datasets/MNIST_M/DAPCA_error.m
@@ -1,12 +1,15 @@
-load('datasets/MNIST_M/MNIST_test_small.mat');
+dataset = 'test_small_PCA'
+load(['datasets/MNIST_M/MNIST_',dataset,'.mat']);
 X1 = data;
 y1 = labels;
+load(['datasets/MNIST_M/MNIST_M_',dataset,'.mat']);
+X2 = data;
+y2 = labels;
+disp(['X1 shape',num2str(size(X1))])
+disp(['X2 shape',num2str(size(X2))])
 
-[v,u] = pca(X1); scatter(u(:,1),u(:,2),10,y1,'filled');
-
-alpha = 1; 
-[ V, D ] = SupervisedPCA(X1, y1, 2, alpha); u = X1*V; 
-figure; scatter(u(:,1),u(:,2),10,y1,'filled');
-
-[V1, D1, PX, PY] = DAPCA(X1, y1, [], 2, 'alpha', alpha);
-figure;  scatter(PX(:,1), PX(:,2), 10, y1, 'filled'); 
+alpha = 5;
+beta = 1;
+gamma = 0.02;
+kNN = 5;
+[V2, D2, PXd, PYd] = DAPCA(X1, y1, X2, 2, 'alpha', alpha, 'beta', beta, 'gamma', gamma,'kNN',kNN);
